3be3ab8bef4467e3289da7ef2118c1a629727f97
Bodor Gergely <bodorgergely91@gmail.com>
2017-05-21 00:59:04 +0200

    added login and navigation for basic components

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { LoginService } from '../login/login.service';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+
+    constructor(private ls: LoginService) {
+    }
+
+    public onLogout(): void {
+        this.ls.logout();
+    }
+
+    public isAdmin(): boolean {
+        return this.ls.isAdmin;
+    }
+
+    public getProfilePictureUrl(): string {
+        return localStorage.getItem('photoUrl');
+    }
+}
